e3837f1e8f9630611804dabd131810d7ff2d6370
aniket panchal <aniketpanchal07@gmail.com>
2024-11-08 00:42:09 +0530

    done with the textfield

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,17 +7,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'BreakFast',
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white, // AppBar color
-        elevation: 0.0,
-        centerTitle: true,
+      appBar: appBar(),
 
-        leading: Container(
+      // body: const Center(
+      //   child: Text(
+      //     'Hello, Flutter!',
+      //     style: TextStyle(
+      //         fontSize: 24, color: Colors.black), // Text style and color
+      //   ),
+      // ),
+
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                  color: const Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0),
+            ]),
+            child: TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(15),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none)),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white, // Scaffold background color
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      // center
+
+      title: const Text(
+        'BreakFast',
+        style: TextStyle(
+            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.white, // AppBar color
+      elevation: 0.0,
+      centerTitle: true,
+
+      // this is left side
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
           decoration: BoxDecoration(
@@ -32,14 +74,25 @@ class HomePage extends StatelessWidget {
         ),
       ),
 
-      body: const Center(
-        child: Text(
-          'Hello, Flutter!',
-          style: TextStyle(
-              fontSize: 24, color: Colors.black), // Text style and color
-        ),
-      ),
-      backgroundColor: Colors.white, // Scaffold background color
+      // this is right side
+      actions: [
+        GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              width: 37,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+            )),
+      ],
     );
   }
 }
